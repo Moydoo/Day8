fede3c39d3786cbf378f494f3534e060929720f6
Michal <m.stefanski98@gmail.com>
2023-05-17 15:41:25 +0200

    Add project files.

---
+++ P01NewASPNetWebApp/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="P01NewASPNetWebApp.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title></title>
+</head>
+<body>
+	<form id="form1" runat="server">
+		<h1>Welcome to My ASP.NET Web Application</h1>
+		<p>This is a simple web application built using ASP.NET.</p>
+		<asp:TextBox ID="txtNumber1" runat="server"></asp:TextBox>
+		<asp:TextBox ID="txtNumber2" runat="server"></asp:TextBox>
+		<br />
+		<asp:Button ID="Calculate" OnClick="btnCalculate_Click" Text="Calculate" runat="server" />
+		<br />
+		<asp:Label ID="lblResult" Text="Your result will be visible here :)" runat="server" />
+
+
+	</form>
+</body>
+
+
+</html>
---
+++ P01NewASPNetWebApp/VolleyballPlayers.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P01NewASPNetWebApp
+{
+	public partial class VolleyballPlayers : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			string connectionString = "Server=(localdb)\\mssqllocaldb;Database=VolleyballDatabase;Integrated Security=True;";
+
+			using (SqlConnection connection = new SqlConnection(connectionString))
+			{
+
+				connection.Open();
+
+				try
+				{
+					//SELECT
+					using (SqlCommand command = new SqlCommand("SELECT Id, First_Name, Last_Name FROM Players", connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								Response.Write($"Id: {reader.GetInt32(0)}, First name: {reader.GetString(1)}, Last name: {reader.GetString(2)}");
+								Response.Write("<br>");
+							}
+						}
+					}
+
+				}
+				catch (SqlException ex)
+				{
+					Response.Write("An error occurred while connecting to the database or executing the command: " + ex.Message);
+				}
+				catch (Exception ex)
+				{
+					Response.Write("An unexpected error occurred: " + ex.Message);
+				}
+
+			}
+		}
+	}
+}
